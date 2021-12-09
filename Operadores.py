7c96dd7d13d9757de5ba501fdcb486c6494e01b1
Sara Acuña <acunabenavidessaraelis@gmail.com>
2021-12-09 00:45:18 -0500

    Operadores asitmeticos y sus prioridades

---
+++ Operadores.py
@@ -0,0 +1,34 @@
+#Suma
+num1=100
+num2=23
+resultado=num1+num2
+print("Mi suma es: ", resultado)
+
+#Resta
+resultado=num1-num2
+print("Mi resta es: ", resultado)
+
+#Multiplicacion 
+resultado=num1*num2
+print("Mi multiplicacion es: ", resultado)
+
+#Division 
+resultado=num1/num2
+print("Mi division es: ", resultado)
+
+#Division resultdo entero
+resultado=num1//num2
+print("Mi division es: ", resultado)
+
+#Residuo de la division: Modulo
+resultado=num1%num2
+print("Mi residuo es: ", resultado)
+
+#Exponenciacion
+resultado=num1**num2
+print("Mi exponenciacion es: ", resultado)
+
+
+#Ejemplo
+ejemplo= 3**3*((13/5)-(2*4))
+print("Mi ejemplo da como resultado: ", ejemplo)
