5a7552366701c7252b5d199093362bc6b5b72f5b
Sara Acuña <acunabenavidessaraelis@gmail.com>
2021-12-09 00:31:26 -0500

    Asignacion y tipado diamico

--- prueba.py
+++ prueba.py
@@ -1,1 +1,11 @@
-print("Mi nombre es: \n Sara")
+#Asignacion y operacion de variables
+numero1=21
+numero2=210
+suma=numero1+numero2
+print("El resultado es", suma)
+
+#Tipado dinamico: Asignar a una variable diferentes tipos de datos
+valor=10
+print(valor)
+valor=True
+print(valor)
